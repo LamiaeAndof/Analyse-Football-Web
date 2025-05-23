d3add7d025795942cdb9a85c78ec6ac518dd9cd3
Lamiae Andof <lamiae.andof@etu.uae.ac.ma>
2025-05-23 01:17:19 +0100

    💡 First push: full Ballon d'Or prediction project

--- 01_scraping/scrape_titles.py
+++ 01_scraping/scrape_titles.py
@@ -2,13 +2,13 @@ import pandas as pd
 import os
 
 # Définir le chemin relatif vers le fichier Excel simulé
-input_path = "data/raw/titles_2025_global.xlsx"
-output_path = "data/raw/titles_2025.csv"
+input_path = "data/titles_2025_global.xlsx"
+output_path = "data/rtitles_2025.csv"
 
 
 # Vérification de l'existence du fichier
 if not os.path.exists(input_path):
-    print("❌ Le fichier titles_2025_global.xlsx n'a pas été trouvé dans data/raw/")
+    print("❌ Le fichier titles_2025_global.xlsx n'a pas été trouvé dans data/")
 else:
     # Charger le fichier Excel
     df_titles = pd.read_excel(input_path)
